d22863e3584be3fd9cd3f103c0c5309236be8c1c
c-success <39614210+c-success@users.noreply.github.com>
2018-05-25 12:58:08 +0530

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,9 @@
+     **hc-creds-secret.yaml**
+       apiVersion: v1
+       kind: Secret
+       metadata:
+       name: Name of the Kubernetes secret 
+       type: Opaque
+       data:
+       username: Tenant User Name
+       password: Tenant password
